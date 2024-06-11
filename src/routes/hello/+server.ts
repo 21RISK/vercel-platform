4d0f7eb4692bd76a9a2014601ba78540ec678b3b
Alex Bjørlig <alexbjorlig@icloud.com>
2024-06-11 22:05:40 +0200

    one more iteration

--- src/routes/hello/+server.ts
+++ src/routes/hello/+server.ts
@@ -4,6 +4,7 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (event) => {
 
+
     console.log('is platform here?', event.platform); // Why is event.platform undefined here?
-    return json({ message: 'GET request to the homepage' });
+    return json({ message: 'GET request to the homepage', platformDefined: !!event.platform });
 }
