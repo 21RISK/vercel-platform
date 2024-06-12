b03776924c70ae1124285e9915ae5e6ffdadca60
Alex Bjørlig <alexbjorlig@icloud.com>
2024-06-12 09:26:54 +0200

    and on vercel?

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle: Handle = async (e) => {
 
+	console.log('is it here?', e);
+	const { event, resolve,  } = e;
     console.log('hooks-server:', {platform: event.platform});
 	const response = await resolve(event);
 	return response;
